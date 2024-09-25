9f10808ff221d8a05b198612f50757aea62772ce
cactus-jacck <aayushnag84@gmail.com>
2024-09-25 15:36:57 +0530

    implement h2 database type authentication

--- src/main/java/com/aayush/spring_security_demo/config/SecurityConfiguration.java
+++ src/main/java/com/aayush/spring_security_demo/config/SecurityConfiguration.java
@@ -1,21 +1,36 @@
 package com.aayush.spring_security_demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration
+{
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfiguration(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")                      // Only ADMIN role can access /admin/*
                 .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")     // Only USER role can access /user/*
                 .antMatchers("/**").permitAll()                                 // Allow anyone to access /*
@@ -23,24 +38,31 @@ public class SecurityConfiguration {
                 .and()
                 .formLogin()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")                  // Disable CSRF for H2 console
+                .and()
+                .headers().frameOptions().sameOrigin();                                    // Allow H2 console to be displayed in a frame
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}pass")
                 .roles("USER")
                 .build();
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("adminpass")
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}pass")
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
     }
 }
